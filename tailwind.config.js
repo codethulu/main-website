2a3733df3b26e2dd9e246def60d598a5ef7e7ebc
Brendan Bell <45674799+thulhu@users.noreply.github.com>
2021-07-10 03:53:47 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'oswald': ['oswald', 'sans-serif'],
+        'odibee': ['"odibee sans"', 'sans-serif']
+      },
+      colors:{
+        'Tgray': colors.trueGray,
+  
+      }
+    },
+    
+  },
+  variants: {
+    extend: {
+        backdropBlur: ['hover', 'focus'],
+        display: ["group-hover"],
+        opacity: ["group-hover"],
+        translate: ["group-hover"],
+        transform: ["group-hover"],
+        width: ["group-hover", "hover"],
+        height: ["group-hover", "hover"],
+        padding: ["group-hover", "hover"],
+        animation: ["group-hover", "hover"],
+        scale: ["group-hover", "hover"],
+    },
+  },
+  plugins: [],
+}
