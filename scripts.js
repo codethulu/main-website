1febc2dcf826d2180259e2344e69b041fdb955b0
codethulu <brendanbell2001@hotmail.com>
2021-07-26 16:37:03 +0100

    button visibility changed to work through classes

--- scripts.js
+++ scripts.js
@@ -2,29 +2,18 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
-    document.getElementById("contactButton").style.opacity = "0%"; 
+    document.getElementById("contactButtonB").classList.remove("button-active");
+    document.getElementById("contactButtonB").classList.add("button-inactive");
 
   } else if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
-    // document.getElementById("header").style.padding = "0px";    
-    // document.getElementById("header").style.opacity = "50%";   
-    // document.getElementById("header").style.fontSize = "20px";   
-    
-    document.getElementById("contactButtonA").style.opacity = "0%"; 
-    document.getElementById("contactButtonA").style.pointerEvents = "none";
-    
-    document.getElementById("contactButtonB").style.opacity = "90%"; 
-    document.getElementById("contactButtonB").style.pointerEvents = "auto";
-
-
+    document.getElementById("contactButtonB").classList.add("button-active");
+    document.getElementById("contactButtonB").classList.remove("button-inactive");
+    document.getElementById("contactButtonA").classList.add("button-inactive");
+    document.getElementById("contactButtonA").classList.remove("button-active");
   } else {
-    // document.getElementById("header").style.padding = "5px";
-    // document.getElementById("header").style.opacity = "100%"; 
-    // document.getElementById("header").style.fontSize = "40px";      
-
-    document.getElementById("contactButtonB").style.opacity = "0%"; 
-    document.getElementById("contactButtonA").style.pointerEvents = "auto";
-    document.getElementById("contactButtonB").style.pointerEvents = "none";
-    document.getElementById("contactButtonA").style.opacity = "100%"; 
- 
+    document.getElementById("contactButtonA").classList.add("button-active");
+    document.getElementById("contactButtonA").classList.remove("button-inactive");
+    document.getElementById("contactButtonB").classList.add("button-inactive");
+    document.getElementById("contactButtonB").classList.remove("button-active");
   }
 }
